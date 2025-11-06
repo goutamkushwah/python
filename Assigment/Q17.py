86163d518a2af93e6c31c179466f0834797330ee
goutam <goutam.kushwah2003@gmail.com>
2025-11-06 17:59:30 +0530

    Q17 soluation

--- Assigment/Q17.py
+++ Assigment/Q17.py
@@ -1,2 +1,13 @@
 # Date - 31/10/2025
 # WAP for implementing packages in python
+# Importing functions from package
+from arithmetic import add, sub
+
+def main():
+    x = 15
+    y = 10
+    print("Addition:", add(x, y))
+    print("Subtraction:", sub(x, y))
+
+if __name__ == "__main__":
+    main()
