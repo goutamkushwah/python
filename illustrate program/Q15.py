6e8bf6024257fb3068415431ed8928453f0af21f
goutam <goutam.kushwah2003@gmail.com>
2025-12-15 14:11:35 +0530

    DSA

---
+++ illustrate program/Q15.py
@@ -0,0 +1,21 @@
+# Date - 24/11/2025
+# Python program to count vowels, consonants and blank spaces in a string
+
+s = "my name is goutam kushwah"
+vowels = 0
+consonants = 0
+blanks = 0
+
+vol = ["a","e","i","o","u","A","E","I","O","U"]
+bla = [" "]
+for ch in s:
+    if ch in vol:
+        vowels += 1
+    elif ch in bla:
+        blanks += 1
+    else:
+        consonants += 1
+
+print("Vowels =", vowels)
+print("Consonants =", consonants)
+print("Blank spaces =", blanks)
