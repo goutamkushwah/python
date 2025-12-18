135b18e5ffd5675d78198891b30fd03c9160aeff
goutam <goutam.kushwah2003@gmail.com>
2025-12-18 13:09:09 +0530

    Chapter -5 Sorting Algo

---
+++ DSA/Chapter-5/BubbleSort.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(0,len(arr)-i-1): 
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+
+    return arr    
+    
+arr=[1,23,5,4,10,9,8,7,6,2]    
+print("Unsorted Array:",arr)
+sorted_arr=bubble_sort(arr)
+print("Sorted Array:",sorted_arr)
+
+  
