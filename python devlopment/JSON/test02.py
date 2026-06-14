ed5e84899c278971aa4746b768f86fef3444d086
goutam <goutam.kushwah2003@gmail.com>
2026-06-14 16:08:03 +0530

    JSON

---
+++ python devlopment/JSON/test02.py
@@ -0,0 +1,19 @@
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# print(json.dumps(x))
+# print(json.dumps(x, indent=4))
+a= json.dumps(x, indent=4, separators=(". ", " = "))
+print(a)
